5d25018fbc1ffa7793dad514766d28c2752c1f71
HinLeung622 <56083013+HinLeung622@users.noreply.github.com>
2021-06-12 18:26:48 +0800

    v0.4.0
    - decoupled the custom SFH array so each SFH component now has their own custom SFH when passed to bagpipes, meaning individual metallicity
    - changed slider priorities accordingly
    - added notice when importing package about the whole deal concerning no multinest bagpipes installations
    - minor fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pipes-vis",
-    version="0.3.1",
+    version="0.4.0",
     author="Ho-Hin Leung",
     author_email="hhl1@st-andrews.ac.uk",
     description="Small interactive GUI/visualizer tool for SPS spectra",
